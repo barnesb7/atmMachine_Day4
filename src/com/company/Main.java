81ee7e10beac322cab5b38e9e775201bf07dee27
Brian B <brianb@MacBook-Pro-4.local>
2019-01-17 18:01:17 -0500

    Adding basic current balance logic, withdraw logic, and continue option logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
-        String promptAtStartup = "Would you like to: ";
+        String promptAtStartup = "What would you like to do?";
         String optionOne = "1 - Check Account Balance";
         String optionTwo = "2 - Withdraw Funds";
         String optionThree = "3 - Deposit Funds";
         String lineBreak = "\n";
+        double accountBalance = 100.50;
         boolean likeToContinue = true;
 
         do{
@@ -19,13 +20,30 @@ public class Main {
             int userIntegerInputAtStart= userInput.nextInt();
 
                 if(userIntegerInputAtStart == 1){
-
+                        System.out.println("Your current balance is: " + accountBalance);
                 } else if (userIntegerInputAtStart == 2){
+                        System.out.println("Please enter withdraw amount in multiples of 10");
+
+                         int amountToWithdraw= userInput.nextInt();
+
+                         accountBalance -= amountToWithdraw;
+                         System.out.println("Please take your " + amountToWithdraw + " your current balance is " + accountBalance);
 
                 } else if (userIntegerInputAtStart == 3){
 
                 }
 
+        String continuePrompt = "Would you like to make another transaction?";
+        String continueOptionText = " 1 - Yes";
+        String quitOptionText = "2 - No";
+
+        System.out.println(continuePrompt + lineBreak + continueOptionText + lineBreak +  quitOptionText);
+        int userWantsToContinue = userInput.nextInt();
+
+        if(userWantsToContinue == 2){
+            likeToContinue = false;
+            System.out.println("Thank you, goodbye.");
+        }
 
         }while(likeToContinue);
 
